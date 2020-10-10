668bdf9faf2ff160ef4e94ea028fdc52535a6228
Dhandare-Abhishek <aadhandare5@gmail.com>
2020-10-10 22:25:33 +0530

    Recipee App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect , useState} from 'react';
 import './App.css';
+import Recipe from "./Recipe.";
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "e90d55b9";
+  const APP_KEY = "ce4f6ebd9fe2588defe267c81be3d811";
+
+  const [recipes, setRecipes] = useState([]);
+
+// test
+  useEffect(() => {
+    getRecipee();
+  }, []
+  );
+
+const getRecipee = async() => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+  setRecipes(data.hits);
+}
+
+  return(
     <div className="App">
-      <h1>My React App</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+    
+    {recipes.map(recipe => (
+      <Recipe 
+      key = {recipe.recipe.label}
+      title = {recipe.recipe.label}
+      calories = {recipe.recipe.calories}
+      image = {recipe.recipe.image}
+      />
+
+    ))}
     </div>
   );
-}
+};
 
 export default App;
