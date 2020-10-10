a5d14265885406288a7db3c1f845ded5f4b7b7b8
Dhandare-Abhishek <aadhandare5@gmail.com>
2020-10-10 22:17:10 +0530

    Updates

---
+++ precache-manifest.9d6568981ecb02a6b2d3f5669f9d56d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66c8b262e0e6da03641822342449d96",
+    "url": "/ReactRecipie-App/index.html"
+  },
+  {
+    "revision": "9672c83e5b6424c3aa39",
+    "url": "/ReactRecipie-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53384ced8f44d5e808a5",
+    "url": "/ReactRecipie-App/static/js/2.53ca6d38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRecipie-App/static/js/2.53ca6d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9672c83e5b6424c3aa39",
+    "url": "/ReactRecipie-App/static/js/main.fceadb84.chunk.js"
+  },
+  {
+    "revision": "6e5d553195149b227de1",
+    "url": "/ReactRecipie-App/static/js/runtime-main.b4c9ec96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactRecipie-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
